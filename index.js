1a8acc53104211371c1a1cf68f1e9841b4dc3cc4
blakescode <rydyell8@gmail.com>
2021-10-07 08:02:38 -0400

    3.3, get single person

--- index.js
+++ index.js
@@ -4,44 +4,54 @@ const app = express()
 app.use(express.json())
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
 ]
 
 app.get('/', (request, response) => {
-    response.send('<h1>try /api/persons</h1>')
+  response.send('<h1>try /api/persons</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(p => p.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
 })
 
 app.get('/info', (request, response) => {
-    const requestTime = new Date()
-    response.send(
-        `
-        <p>Phonebook has info for ${persons.length} people</p>
-        <p>${requestTime.toString()}</p>
-        `
-    )
+  const requestTime = new Date()
+  response.send(
+      `
+      <p>Phonebook has info for ${persons.length} people</p>
+      <p>${requestTime.toString()}</p>
+      `
+  )
 })
 
 const PORT = 3001
